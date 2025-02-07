ec12a5359507796469f8d6f89fb8370c8aa8aa74
Anmar PC <aj@anmarjarjees.com>
2025-02-06 19:12:56 -0500

    update

--- week05/node-final/utilities1.js
+++ week05/node-final/utilities1.js
@@ -10,15 +10,11 @@ Link: https://github.com/anmarjarjees/node.js-start/tree/main/5.modules
 // list of functions:
 // these functions need to be exported:
 // require() => exports
-exports.checkEvenOdd = (number) => {
-  // Function to check if a number is even or odd
-  if (number % 2 === 0) return "Even"; // Checks divisibility by 2
-  else return "Odd"; // Returns "Odd" if not even
-}
 
-// testing:
-//   console.log(checkEvenOdd(7));
-
+// 5! = 5 * 4 * 3 * 2 * 1 
+// 5! =  1 * 2 * 3 * 4 * 5
+// Classical way of function:
+// exporting function getFactorial(number)
 exports.getFactorial = (number) => {
   // Function to calculate the factorial of a number
   let factorial = 1; // Initialize factorial result
@@ -30,3 +26,33 @@ exports.getFactorial = (number) => {
 
 // testing:
 //   console.log(getFactorial(5));
+
+// The Arrow functions:
+// exporting checkEvenOdd = (number) => { }
+exports.checkEvenOdd = (number) => {
+  // Function to check if a number is even or odd
+  if (number % 2 === 0) return "Even"; // Checks divisibility by 2
+  else return "Odd"; // Returns "Odd" if not even
+}
+
+// testing:
+// console.log(checkEvenOdd(7));
+
+// Or in two steps:
+// Step1: Creating the function:
+const findTotal = (tempArray) => {
+  let total = 0;
+  for (let i = 0; i < tempArray.length; i++) {
+    total += tempArray[i];
+  }
+  return total;
+}
+
+// Step1: Assigning the function to a variable to be exported:
+exports.findTotal = findTotal;
+
+// Exporting variables:
+const provinceName = "Ontario";
+exports.provinceName = provinceName;
+
+
